cdcc4a193686a9a2722820363bb9bb7475df7273
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-10-15 15:44:03 +0900

    todo first

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ function App() {
 
   const [toDo, setToDo] = useState("");
   const onChange = (event) => setToDo(event.target.value);
-  console.log(toDo);
-  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    }
+    setToDo("");
+  };
+ 
+
   return (
     <div>
-      <input 
-        onChange={onChange}
-        value={toDo}
-        type="text" 
-        placeholder="Write you todo.." 
-      />
-
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text" 
+          placeholder="Write you todo.." 
+        />
+      <button>Add to Do</button>
+      </form>  
     </div>
   );
 }
