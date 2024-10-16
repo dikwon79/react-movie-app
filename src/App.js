dc3108abfb4edbdc59aee400039862fc92d8b279
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-10-16 13:34:52 +0900

    todo part2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { useState } from "react";
 function App() {
 
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
   const onChange = (event) => setToDo(event.target.value);
+  
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === ""){
       return;
     }
+    
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
  
 
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input 
           onChange={onChange}
@@ -24,6 +30,11 @@ function App() {
         />
       <button>Add to Do</button>
       </form>  
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+      
     </div>
   );
 }
